e0c483f0329f4ce21cb4e6d65f48ecc771638dfb
Ruckaiya <ruckaiya.awf5@gmail.com>
2020-01-03 17:47:35 +0600

    second commit

--- studio/settings.py
+++ studio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-f)ku7(o9(8)0%&sbf7du_myzw67nz^1_)hgbeku--y&13%ld0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -121,9 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
 
 LOGIN_REDIRECT_URL = '/classes/'
